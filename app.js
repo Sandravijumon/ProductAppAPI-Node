0ee23f1d9cff9e3016d17bdff030cbbe5dc4aecb
Sandravijumon <sandravijumon15@gmail.com>
2024-06-10 22:32:31 +0530

    product created

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 const cors=require("cors")
 
 const app=express()
-app.use(cors)
+app.use(cors())
 
 app.get("/",(req,res)=>{
     res.send("Welcome to my home page")
