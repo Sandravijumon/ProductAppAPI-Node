ecf1a67f3327b69f2a10489730706511bd131802
Sandravijumon <sandravijumon15@gmail.com>
2024-06-10 23:05:15 +0530

    Product app api created

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ app.get("/",(req,res)=>{
     res.send("Welcome to my home page")
 })
 
-app.get("/search",(req,res)=>{
-    res.send("Welcome to search page")
-})
-
-app.get("/dlt",(req,res)=>{
-    res.send("Welcome to delete page")
-})
 app.get("/view",(req,res)=>{
     res.send("Welcome to view page")
 })
